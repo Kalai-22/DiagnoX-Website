d8a52a8d4a39de1767a57b59561aa78de0018e65
Kalai <kalaiarasu14082006@gmail.com>
2025-12-11 17:16:28 +0530

    Updates

--- assets/chevron-right-ChuCDEmv.js
+++ assets/chevron-right-ChuCDEmv.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-CzAzaIEk.js";/**
+import{c as o}from"./index-XW-b9ei9.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
